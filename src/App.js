adc38e9ee06007499e3ee92de7d267bd6487243c
laurageeme <registro.laura@gmail.com>
2020-04-22 15:40:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-useless-constructor */
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
